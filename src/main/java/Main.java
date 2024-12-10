14435dac469e8933b60e928e8b1461216e0c954b
TamirFeldenblum <tfeldenblum@frba.utn.edu.ar>
2024-12-10 15:52:06 -0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,23 @@ import io.javalin.plugin.rendering.JavalinRenderer;
 import utils.HandlebarsRenderer;
 import Controller.AlumnoController;
 
+
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
-            config.addStaticFiles("/public", Location.CLASSPATH); // Archivos estáticos
+         //   config.addStaticFiles("/public", Location.CLASSPATH); // Archivos estáticos
             config.enableDevLogging(); // Logs detallados
         }).start(7000);
 
+        System.out.println("MAIN TAMIR: ");
 
         // Registrar el renderizador Handlebars
         JavalinRenderer.register(new HandlebarsRenderer(), ".hbs");
 
+
         // Rutas
         app.get("/alumnos", AlumnoController.listarAlumnos); // Listar alumnos
-        app.get("/asignaturas/:legajo", AlumnoController.asignaturasPorAlumno);
+        app.get("/asignaturas/{legajo}", AlumnoController.asignaturasPorAlumno);
 
 
     }
